509cebfecc1e5e8d40addb8a5e7d9476bca75a62
Maarten <maartendeschrijver@gmail.com>
2020-03-23 14:52:12 +0100

    [DEV-717] Switch app to new PremisEvents
    - remove `get_event_and_fragment_id` fn in lieu of PremisEvents
    - change logic to work with the new class
    - add one unit test for `generate_vrt_xml`

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from io import BytesIO
+
+from lxml import etree
+
 from tests.resources import single_premis_event
-from app.app import get_event_and_fragment_id
+from app.app import (
+    generate_vrt_xml
+)
 
 
-def test_single_get_event_and_fragment_id():
-    (event, fragment_id) = get_event_and_fragment_id(single_premis_event)
-    assert event == "EXPORT"
-    assert fragment_id == "a1b2c3"
+def test_generate_vrt_xml():
+    pid = 'a1b2c3'
+    object_key = 'an_object_key'
+    xml = generate_vrt_xml(pid, object_key)
+    tree = etree.parse(BytesIO(xml.encode('utf-8')))
+    assert tree.xpath('/m:essenceArchivedEvent/m:pid/text()', namespaces={"m":"http://www.vrt.be/mig/viaa/api"})[0] == pid
+    assert tree.xpath('/m:essenceArchivedEvent/m:file/text()', namespaces={"m":"http://www.vrt.be/mig/viaa/api"})[0] == object_key
