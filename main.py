a94c7f6119af8541012528f32a294bcc6ef3aeae
Mattias.Poppe <poppe.mattias@gmail.com>
2021-02-25 15:56:49 +0100

    DEV-1440 Make Uvicorn logging more uniform
    Messages logged by Uvicorn are actually logged twice: once by the Uvicorn
    logger and once by the logger of the application. The format of the Uvicorn
    loggers is also not in JSON format, so remove the handlers of those loggers.
    
    Disable the access logs as logging of the health check pollutes the logs
    too much.
    
    Set the logging level of Uvicorn to the level defined in the config.yml.

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import logging
 
-import uvicorn
+from uvicorn import Config, Server
+
+from viaa.configuration import ConfigParser
+
+
+cfg_log_level = ConfigParser().config["logging"]["level"]
+# Uvicorn expects lowercase string or integer as the logging level.
+LOG_LEVEL = cfg_log_level.lower() if isinstance(cfg_log_level, str) else cfg_log_level
 
 if __name__ == "__main__":
-    uvicorn.run('app.app:app', host='0.0.0.0', port=8080)
+    server = Server(
+        Config(
+            "app.app:app",
+            host="0.0.0.0",
+            port=8080,
+            access_log=False,
+            log_level=LOG_LEVEL,
+        ),
+    )
+
+    # Remove the Uvicorn logging handlers, as our own loggers log in a JSON format.
+    # Just remove them all although the access handler is already removed by "access_log=False".
+    for name, logger in logging.root.manager.loggerDict.items():
+        if name.startswith("uvicorn"):
+            logger.handlers = []
+
+    server.run()
